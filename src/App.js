d461b46f33a7a330562e643037c0cd6d9471fd01
AryavJain17 <122171980+AryavJain17@users.noreply.github.com>
2023-09-07 17:40:52 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-import Carousel from './components/Carousel';
-import Cursor from './components/Cursor';
+
+import Home from './components/Home';
+
 import Navbar from './components/Navbar';
-import Popular from './components/Popular';
-import Search from './components/Search';
+
 
 
 function App() {
@@ -12,19 +12,8 @@ function App() {
    <>
   
    <Navbar className="z-50"/>
-   {/* <h1 className='absolute z-40 ml-44 mt-56 text-white font-extrabold text-3xl drop-shadow-3xl shadow-black'>Let's Bring</h1> */}
-  <Cursor />
-  <Search />
-    <Carousel />
-    <h1 className=' text-center font-bold text-3xl mt-10'>Each District is Special</h1>
-    <div className='flex flex-row'>
-
-  <Popular />
-  {/* <Popular />
-  <Popular />
-  <Popular /> */}
-  
-  </div>
+   
+ <Home />
    </>
   );
 }
